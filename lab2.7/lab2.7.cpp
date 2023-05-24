8403135cea49fa44c6f40f9e85e8abc75aa1ea3a
allanati <allanatiaganova@gmail.com>
2023-05-24 15:05:59 +0200

    virtual destructor

--- lab2.7/lab2.7.cpp
+++ lab2.7/lab2.7.cpp
@@ -9,75 +9,101 @@
 Продемонстрируйте, как ваша программа будет выводить на экран площади всех фигур.
 */
 
-class Shape 
+class Shape
 {
-    private:
-        std::string Color;
-        double X, Y;
+private:
+    std::string Color;
+    double X, Y;
 
-    public:
-        Shape(const std::string& color, double x, double y) : Color(color), X(x), Y(y) {};
+public:
+    Shape(const std::string& color, double x, double y) : Color(color), X(x), Y(y) {};
 
-        virtual double getArea() const {      // чтобы можно было переопределить производным классом
-            std::cout << "Area of the shape: ";
-            return 0;
-        }
+    virtual double getArea() const  // чтобы можно было переопределить производным классом
+    {
+        std::cout << "Area of the shape: ";
+        return 0;
+    }
+    virtual ~Shape()
+    {
+        std::cout << "Base destructor called" << std::endl;
+    }
 };
 
-class Circle : public Shape 
+class Circle : public Shape
 {
-    private:
-        double Radius;
+private:
+    double Radius;
 
-    public:
-        Circle(const std::string& color, double x, double y, double radius) : Shape(color, x, y), Radius(radius) {}
+public:
+    Circle(const std::string& color, double x, double y, double radius) : Shape(color, x, y), Radius(radius) {}
 
-        double getArea() const override {
-            std::cout << "Area of the circle: ";
-            return acos(-1) * Radius * Radius;
-        }
+    double getArea() const override {
+        std::cout << "Area of the circle: ";
+        return acos(-1) * Radius * Radius;
+    }
+
+    ~Circle() {
+        std::cout << "Derived (circle) destructor called" << std::endl;
+    }
 };
 
-class Triangle : public Shape 
+class Triangle : public Shape
 {
-    private:
-        double FirstSide, SecondSide, ThirdSide;
+private:
+    double FirstSide, SecondSide, ThirdSide;
 
-    public:
-        Triangle(const std::string& color, double x, double y, double firstSide, double secondSide, double thirdSide) : 
-            Shape(color, x, y), FirstSide(firstSide), SecondSide(secondSide), ThirdSide(thirdSide) {}
+public:
+    Triangle(const std::string& color, double x, double y, double firstSide, double secondSide, double thirdSide) :
+        Shape(color, x, y), FirstSide(firstSide), SecondSide(secondSide), ThirdSide(thirdSide) {}
 
-        double getArea() const override {
-            std::cout << "Area of the triangle: ";
-            double p = (FirstSide + SecondSide + ThirdSide) / 2;
-            return sqrt(p * (p - FirstSide) * (p - SecondSide) * (p - ThirdSide));
-        }
+    double getArea() const override {
+        std::cout << "Area of the triangle: ";
+        double p = (FirstSide + SecondSide + ThirdSide) / 2;
+        return sqrt(p * (p - FirstSide) * (p - SecondSide) * (p - ThirdSide));
+    }
+    
+    ~Triangle()
+    {
+        std::cout << "Derived (triangle) destructor called" << std::endl;
+    }
 };
 
-class Rectangle : public Shape 
+class Rectangle : public Shape
 {
 private:
     double Width, Height;
 
 public:
     Rectangle(const std::string& color, double x, double y, double width, double height) : Shape(color, x, y), Width(width), Height(height) {}
+
     double getArea() const override {
         std::cout << "Area of the rectangle: ";
         return Width * Height;
+    }
+
+    ~Rectangle()
+    {
+        std::cout << "Derived (rectangle) destructor called" << std::endl;
     }
 };
 
 
 int main()
 {
-    Circle ob1("yellow", 0, 0, 5);
-    std::cout << ob1.getArea() << std::endl;
+    Shape* shapes[3] = {};
+    shapes[0] = new Circle("yellow", 0, 0, 5);
+    shapes[1] = new Triangle("green", 0, 0, 30, 40, 50);
+    shapes[2] = new Rectangle("blue", 0, 0, 5, 3);
 
-    Triangle ob2("green", 0, 0, 30, 40, 50);
-    std::cout << ob2.getArea() << std::endl;
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << shapes[i]->getArea() << std::endl;
+    }
 
-    Rectangle ob3("blue", 0, 0, 5, 3);
-    std::cout << ob3.getArea() << std::endl;
+    for (int i = 0; i < 3; i++)
+    {
+        delete shapes[i];
+    }
 
     return 0;
 }
